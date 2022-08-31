413864d8cc60d056f6cd0961920858cbdc0163bd
Heber Silverio <hebersilverio@gmail.com>
2022-08-31 20:42:29 -0300

    Exercício Calculadora

---
+++ exercicios/calculadora/assets/js/main.js
@@ -0,0 +1,18 @@
+function criaCalculadora() {
+   return {
+      display: document.querySelector('.display'),
+
+
+
+
+      inicia() {
+         
+      },
+
+      cliqueBotoes() {}
+
+   };
+} 
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
