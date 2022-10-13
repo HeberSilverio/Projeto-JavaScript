c5301d07322ee79f549aa33dcb8cd9e8ec424ea5
Heber Silverio <hebersilverio@gmail.com>
2022-10-13 14:40:39 -0300

    Novo exercício

---
+++ Exe JavaScript SerFrontend/Problema10-Calculadora/assets/js/main10.js
@@ -0,0 +1,55 @@
+// PROBLEMA 10
+// Fazer uma calculadora
+
+
+let mensagem = '';
+
+let n1 = parseFloat(prompt('Digite um número'));
+
+
+
+function escreve(){
+   const output = document.querySelector('#output');
+   // Mostra mensagem no output
+   output.innerHTML = n1;
+}
+escreve();
+function somar(){
+   let n = parseFloat(prompt('Digite outro número'));
+   n1 = n1 + n;
+   escreve();
+}
+function subtrair(){
+   let n = parseFloat(prompt('Digite outro número'));
+   n1 = n1 - n;
+   escreve();
+}
+function multiplicar(){
+   let n = parseFloat(prompt('Digite outro número'));
+   n1 = n1 * n;
+   escreve();
+}
+function dividir(){
+   let n = parseFloat(prompt('Digite outro número'));
+   n1 = n1 / n;
+   escreve();
+}
+
+
+
+function limparTabuada(){
+   let mensagem = '';
+   output.innerHTML = mensagem;
+}
+
+// let i = 1;
+//       // Enquanto índice <= 1000
+//       // Concatena na variavel mensagem
+//    while(i <= 10){
+//       mensagem += n + ' x ' + i +' = '+ (n*i) + '<br>';
+//       i++;
+//    }
+
+
+
+
