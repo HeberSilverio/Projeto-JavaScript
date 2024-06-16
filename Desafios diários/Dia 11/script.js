52df801981bc191c795e12b8daff18e434aaa71c
HeberSilverio <hebersilverio@gmail.com>
2024-06-16 19:16:49 -0300

    altera casa decimais

--- Desafios diários/Dia 11/script.js
+++ Desafios diários/Dia 11/script.js
@@ -5,9 +5,9 @@ function clock  () {
     let seconds = document.getElementById('seconds');
     let amPm = document.getElementById('amPm');
     
-    let h = new Date().getHours();
+    let h = new Date().getHours().toString().padStart(2, '0');
     let m = new Date().getMinutes().toString().padStart(2, '0');
-    let s = new Date().getSeconds();
+    let s = new Date().getSeconds().toString().padStart(2, '0');
     let am = 'AM';
 
     if (h > 12) {
