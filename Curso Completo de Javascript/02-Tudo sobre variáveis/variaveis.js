06ae2991ee50d5653a74497000e22732a366c817
HeberSilverio <hebersilverio@gmail.com>
2024-08-21 07:03:00 -0300

    aula 0

--- Curso Completo de Javascript/02-Tudo sobre variáveis/variaveis.js
+++ Curso Completo de Javascript/02-Tudo sobre variáveis/variaveis.js
@@ -1,12 +1,62 @@
 "use strict";
 
-// let user = 'John', age = 25, message = 'Hello';
-// const user
+// let propName = "age";
 
-// console.log(user + ' ' + age + ' ' + message); // John 25 Hello
+// let user = {
+//   name: "John",
+//   [propName]: 30 // A propriedade "age" é computada a partir da variável propName
+// };
 
-let text = "The ' character is called an apostrophe.";
+// console.log(user.age); // 30
+// console.log(user[propName]); // 30
+// console.log(typeof user.age); // number
+// console.log(typeof user[propName]); // number
+// console.log(typeof propName); // string
 
-// console.log(text); // João Silva 25 Engenheiro
-// console.log(typeof text); // string
-console.log(typeof x !== 'undefined');
+// let fruit = prompt("Which fruit to buy?", "apple");
+
+// let bag = {
+//   apple: "vermelha", // the name of the property is taken from the variable fruit
+//   orange: "laranja"
+// };
+
+// let resposta = fruit;
+
+
+// if (bag[fruit]) { 
+//   alert( bag[fruit] );
+// } else {  
+//   bag[resposta] = "fruta sem cor";
+// alert( resposta + " " + bag[resposta] ); 
+// }
+ 
+// function makeUser(name, age) {
+//   return {
+//     name, // same as name: name
+//     age,  // same as age: age
+//     // ...
+//   };
+// }
+
+// let user = makeUser("John", 30);
+// console.log(user.name); // John
+
+
+
+//Crie um objeto vazio user.
+let user = {};
+
+//Adicione a propriedade name com o valor John.
+user.name = "John 3.16";
+
+//Adicione a propriedade surname com o valor Smith.
+user.surname = "Deus amou o mundo";
+
+//Altere o valor de name para Pete.
+user.name = "John";
+
+//Remova a propriedade name do objeto.
+delete user.name;
+
+console.log(user);
+console.log(user.surname);
--- Curso Completo de Javascript/05 Estrutura de dados/js/README.md
+++ Curso Completo de Javascript/05 Estrutura de dados/js/README.md
@@ -21,3 +21,17 @@ myObject.property; // 'Value!'
 // bracket notation
 myObject["obnoxious property"]; // [Function]
 ```
+
+#### Teste de existência de propriedade, operador “in”
+Uma característica notável dos objetos em JavaScript, em comparação com muitas outras linguagens, é que é possível acessar qualquer propriedade. Não haverá erro se a propriedade não existir!
+
+Ler uma propriedade não existente retorna apenas undefined. Então podemos testar facilmente se a propriedade existe:
+
+let user = {};
+
+alert( user.noSuchProperty === undefined ); // true means "no such property"
+Há também um operador especial "in"para isso.
+
+A sintaxe é:
+
+"key" in object
