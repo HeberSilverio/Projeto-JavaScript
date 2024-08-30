e17f960e44abc017e74c0898a243539403f38e62
HeberSilverio <hebersilverio@gmail.com>
2024-08-30 19:49:16 -0300

    this

--- Curso Completo de Javascript/02-Tudo sobre variáveis/variaveis.js
+++ Curso Completo de Javascript/02-Tudo sobre variáveis/variaveis.js
@@ -144,23 +144,4 @@ let user = {
   isAdmin: false
 };
 
-// create a function and immediately call it with new
-let user = new function() {
-  this.name = "John";
-  this.isAdmin = false;
 
-  // ...other code for user creation
-  // maybe complex logic and statements
-  // local variables etc
-};
-
-
-// create a function and immediately call it with new
-let user = new function() {
-  this.name = "John";
-  this.isAdmin = false;
-
-  // ...other code for user creation
-  // maybe complex logic and statements
-  // local variables etc
-};
