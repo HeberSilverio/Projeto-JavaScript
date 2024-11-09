2d7930a69f617753a7c78ebfcb2908c91a4145cb
HeberSilverio <hebersilverio@gmail.com>
2024-11-09 14:21:06 -0300

    Explicação de código

--- Curso Completo de Javascript/Exercicios praticos/estado de botao/main.js
+++ Curso Completo de Javascript/Exercicios praticos/estado de botao/main.js
@@ -1,14 +1,31 @@
-let btn = document.querySelector("button");
-let txt = document.querySelector("p");
+// let btn = document.querySelector("button");
+// let txt = document.querySelector("p");
 
-btn.addEventListener("click", updateBtn);
+// btn.addEventListener("click", updateBtn);
 
-function updateBtn() {
-  if (btn.textContent === "Iniciar máquina") {
-    btn.textContent = "Parar máquina";
-    txt.textContent = "A máquina iniciou!";
-  } else {
-    btn.textContent = "Iniciar máquina";
-    txt.textContent = "A máquina está parada.";
-  }
-}
+// function updateBtn() {
+//   if (btn.textContent === "Iniciar máquina") {
+//     btn.textContent = "Parar máquina";
+//     txt.textContent = "A máquina iniciou!";
+//   } else {
+//     btn.textContent = "Iniciar máquina";
+//     txt.textContent = "A máquina está parada.";
+//   }
+// }
+
+
+const codigoBase = 'SKU999';
+// const codigoFormatado = codigoBase.replace(/^SKU(\d+)/, (_, numero) => 'SKU' + numero.padStart(4, '0'));
+// console.log(codigoFormatado);
+
+
+let numeroCapturado; // Declara a variável para armazenar o valor capturado
+
+const codigoFormatado = codigoBase.replace(/^SKU(\d+)/, (_, numero) => {
+    numeroCapturado = numero; // Atribui o valor de `numero` a `numeroCapturado`
+    return 'SKU' + numero.padStart(4, '0'); // Continua com o formato desejado
+});
+
+console.log(codigoFormatado); // Exibe o código formatado, por exemplo, "SKU0001"
+console.log(numeroCapturado); // Exibe o valor capturado, por exemplo, "1"
+
